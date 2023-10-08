af1fc70df323780e1907f3fe1f380ce18136f023
Henrik Helmers <helmers@me.com>
2023-10-08 18:16:20 +0200

    Initial commit

---
+++ MyLibrary/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftBlob",
+    platforms: [.iOS(.v16), .macOS(.v14)],
+    products: [
+        .library(
+            name: "SwiftBlob",
+            targets: ["SwiftBlob"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftBlob"),
+        .testTarget(
+            name: "SwiftBlobTests",
+            dependencies: ["SwiftBlob"]),
+    ]
+)
---
+++ MyLibrary/Sources/SwiftBlob/BlobView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+import Combine
+
+public struct BlobView: View {
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    /// Reasonable range is 0.1 to 1
+    var spikyness: Double
+    var isAnimated: Bool
+
+    @State private var points: AnimatableCGPointVector
+
+    init(spikyness: Double = 0.1, isAnimated: Bool = true) {
+        self.spikyness = spikyness
+        self.isAnimated = isAnimated
+        self.points = BlobView.getPoints(for: spikyness)
+    }
+
+    public var body: some View {
+        BlobShape(controlPoints: points)
+            .aspectRatio(1, contentMode: .fit)
+            .onReceive(timer) { _ in
+                if isAnimated {
+                    withAnimation(.easeInOut(duration: 8)) {
+                        points = BlobView.getPoints(for: spikyness)
+                    }
+                }
+            }
+    }
+
+    static func getPoints(for spikyness: Double) -> AnimatableCGPointVector {
+        let minGrowth = Int(10 - (spikyness * 10))
+        let edges = max(4, Int(spikyness * 50))
+        return BlobShape.createPoints(minGrowth: minGrowth, edges: edges)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.gray
+            .ignoresSafeArea()
+        BlobView(spikyness: 0.1, isAnimated: true)
+            .foregroundColor(.white)
+    }
+    .frame(width: 300, height: 300)
+}
